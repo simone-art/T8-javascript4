cd53e6acbc98c47b9d98074f28f19ce4c4b014dc
simone-art <simoneperiodista3@gmail.com>
2019-11-04 14:56:29 -0300

    realizado el mismo ejercicio pero con un código diferente y máscorto

--- array/ex1/index.js
+++ array/ex1/index.js
@@ -8,21 +8,21 @@ let buttonType = document.getElementById("buttonFin");
 console.log(buttonType)
 
 
-buttonFin.addEventListener("click", function(){
-    let arrAchou = frutas.filter(fruta => fruta === inputType.value)
-    console.log(arrAchou)
-    if (arrAchou.length > 0) {
-        alert(`ACHOU 0 ${inputType.value}`)
+// buttonFin.addEventListener("click", function(){
+//     let arrAchou = frutas.filter(fruta => fruta === inputType.value)
+//     console.log(arrAchou)
+//     if (arrAchou.length > 0) {
+//         alert(`ACHOU 0 ${inputType.value}`)
 
-    }else{
-        alert(`NAO ACHOU 0 ${inputType.value}`)
+//     }else{
+//         alert(`NAO ACHOU 0 ${inputType.value}`)
 
-    }
-});
+//     }
+// });
 
-// if(frutas.find (fruta => fruta === inputType.value)){
-//     alert(`ACHOU 0 ${inputType.value}`)
-// }else{
-//     alert(`NAO ACHOU 0 ${inputType.value}`)
+if(frutas.find (fruta => fruta === inputType.value)){
+    alert(`ACHOU 0 ${inputType.value}`)
+}else{
+    alert(`NAO ACHOU 0 ${inputType.value}`)
 
-// }
+}
