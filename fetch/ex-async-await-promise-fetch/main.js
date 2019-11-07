66e1b6995076d428fb95f796b28c7098ab522d28
simone-art <simoneperiodista3@gmail.com>
2019-11-07 14:27:36 -0300

    finalizado el mismo ejercicio pero con async function

--- fetch/ex-async-await-promise-fetch/main.js
+++ fetch/ex-async-await-promise-fetch/main.js
@@ -3,13 +3,27 @@
 //colocando a imagem criando uma tagg img no body
 //lidando con erros
 
-fetch ("rainbow.jpg").then(response => {
+// fetch ("rainbow.jpg").then(response => {
+//     console.log(response)
+//     return response.blob()
+// }).then(blob => {
+//     console.log(blob)
+//     document.getElementById("rainbow").src =
+//     URL.createObjectURL(blob)
+// // }).catch(error => {
+// //     console.log(error)
+// })
+
+// EL MISMO EJERCICIO CON ASYNC AWAIT
+
+async function pegarArcoIris(){
+    const response = await fetch("rainbow.jpg")
+    const blob = await response.blob()
     console.log(response)
-    return response.blob()
-}).then(blob => {
     console.log(blob)
     document.getElementById("rainbow").src =
     URL.createObjectURL(blob)
-// }).catch(error => {
-//     console.log(error)
-})
+    
+}
+
+pegarArcoIris()
